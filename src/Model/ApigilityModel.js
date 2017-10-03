c1c10da15f243a60b41a9a4d7f7c4e4b09c41efe
Fabio Pellati <fabio.pellati@gmail.com>
2017-10-03 17:40:15 +0200

    aggiustamenti vari: sistemazione del getLink in Model gestione delle collection in SelectModel

--- src/Model/ApigilityModel.js
+++ src/Model/ApigilityModel.js
@@ -1,9 +1,10 @@
 'use strict';
 var Model = require('./Model');
+var Collection = require('../Collection/Collection');
 
 var ApigilityModel = Model.extend({
 
-    linksCollections:{},
+    linksCollections: {},
 
     /**
      * apigility rest espone una chiave con links questo metodo restituisce una Collection che punta a questi link
@@ -11,16 +12,26 @@ var ApigilityModel = Model.extend({
      * @param link
      * @returns {*}
      */
-    getLinkCollection : function(link){
-        var links= this.attributes._links;
-        if (_.isObject(links) && _.has(links,link)) {
+    getLink: function (link) {
+        var links = this.attributes._links;
+        if (_.isObject(links) && _.has(links, link)) {
             var linkObject = _.propertyOf(links)(link);
-            var Collection = ApigilityModel.extend({
-                collection_name: link,
-                url: linkObject.href,
-                model: ApigilityModel
-            });
-            return new Collection();
+            switch (linkObject.type) {
+                case 'collection':
+                    var Link = Collection.extend({
+                        collection_name: link,
+                        url: linkObject.href,
+                        model: ApigilityModel
+                    });
+                    break;
+                case 'entity':
+                    var Link = ApigilityModel.extend({
+                        collection_name: link,
+                        url: linkObject.href
+                    });
+                    break;
+            }
+            return new Link();
         }
         return false;
     },
