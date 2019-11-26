40e13e5ad724afd964e59a6d7452cc03111a13a2
fabio <fabio.pellati@gmail.com>
2019-11-26 11:55:05 +0100

    review

---
+++ src/bundle.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var Module ={};
+Module.Debug = require('./Debug');
+Module.Collection = require('./Collection/Collection');
+Module.Model = require('./Model/Model');
+Module.ApigilityModel = require('./Model/ApigilityModel');
+Module.EnvironmentModel = require('./Model/EnvinonmentModel');
+Module.View = {};
+Module.View.Modal = {};
+Module.View.Modal.Responsive = require('./View/Modal/Responsive');
+
+
+Module.init = function () {
+};
+
+
+
+module.exports = Module;
+
+
